82990b632bc98b184f59d7c96948a48a977c27aa
antipenko <sweetglory.victory@gmail.com>
2016-04-20 07:06:21 +0300

    ver 1.0

---
+++ js/main.js
@@ -0,0 +1,12 @@
+;(function(){
+// imagesLoaded( container, function() {
+// var container = document.querySelector('#container');
+// var msnry = new Masonry( container, {
+//   // Настройки
+//   columnWidth: 300,
+//   itemSelector: '.item',
+//   gutter: 10
+// });
+// });
+})();
+
